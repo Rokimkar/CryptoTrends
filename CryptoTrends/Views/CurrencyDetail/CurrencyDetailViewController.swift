0608d4f0b71fa5bef389040ea44e8a179248d535
Sanchit Kumar Singh <sanchitkumarsingh@gmail.com>
2017-10-05 01:00:16 +0530

    UI changes , applying filter for background image.

--- CryptoTrends/Views/CurrencyDetail/CurrencyDetailViewController.swift
+++ CryptoTrends/Views/CurrencyDetail/CurrencyDetailViewController.swift
@@ -25,6 +25,30 @@ class CurrencyDetailViewController: UIViewController {
         self.cryptoCurrencyDetailTableView.delegate = self
         self.cryptoCurrencyDetailTableView.register(UINib.init(nibName: "CurrencyDetailTableViewCell", bundle: nil), forCellReuseIdentifier: "CurrencyDetailTableViewCell")
         self.cryptoCurrencyDetailTableView.separatorStyle = .none
+        self.cryptoCurrencyDetailTableView.backgroundColor = getAverageColorValueForCurrency()
+        self.view.backgroundColor = UIColor.black
+    }
+    
+    func getAverageColorValueForCurrency() -> UIColor{
+        if let currencyImage = UIImage.init(named: cryptoCurrency!.name!){
+            var bitmap = [UInt8](repeating : 0,count : 4)
+            let context = CIContext(options : nil)
+            let inputImage = CIImage(image:currencyImage)
+            let extent = inputImage!.extent
+            let inputExtent = CIVector(x: extent.origin.x, y: extent.origin.y, z: extent.size.width, w: extent.size.height)
+            let filter = CIFilter(name: "CIAreaAverage", withInputParameters: [kCIInputImageKey: inputImage!, kCIInputExtentKey: inputExtent])!
+            let outputImage = filter.outputImage!
+            let outputExtent = outputImage.extent
+            assert(outputExtent.size.width == 1 && outputExtent.size.height == 1)
+            
+            // Render to bitmap.
+            context.render(outputImage, toBitmap: &bitmap, rowBytes: 4, bounds: CGRect(x: 0, y: 0, width: 1, height: 1), format: kCIFormatRGBA8, colorSpace: CGColorSpaceCreateDeviceRGB())
+            
+            // Compute result.
+            let result = UIColor.init(red: CGFloat(bitmap[0])/255, green: CGFloat(bitmap[1])/255, blue: CGFloat(bitmap[2])/255, alpha: CGFloat(bitmap[3])/255)
+            return result
+        }
+        return UIColor.white
     }
     
     func setNavigationTitleWithAnimation(isSet : Bool){
@@ -148,10 +172,19 @@ extension CurrencyDetailViewController : UITableViewDelegate,UITableViewDataSour
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        if scrollView.contentOffset.y > -20{
-            setNavigationTitleWithAnimation(isSet: true)
-        }else{
-            setNavigationTitleWithAnimation(isSet: false)
+        if let firstTableCell = self.cryptoCurrencyDetailTableView.cellForRow(at: IndexPath.init(row: 0, section: 0)) as? CurrencyDetailTableViewCell{
+            let detailAttributedText = NSMutableAttributedString.init(string: cryptoCurrency!.name!)
+            if scrollView.contentOffset.y > -20{
+                setNavigationTitleWithAnimation(isSet: true)
+            }else if scrollView.contentOffset.y < -64{
+                detailAttributedText.addAttribute(NSAttributedStringKey.font, value: UIFont.systemFont(ofSize: (40+(15*(log(-scrollView.contentOffset.y)-log(64)))), weight: UIFont.Weight.bold), range: NSRange.init(location: 0, length: cryptoCurrency!.name!.count))
+                firstTableCell.detailLabel.attributedText = detailAttributedText
+                setNavigationTitleWithAnimation(isSet: false)
+            }else{
+                detailAttributedText.addAttribute(NSAttributedStringKey.font, value: UIFont.systemFont(ofSize: 40, weight: UIFont.Weight.bold), range: NSRange.init(location: 0, length: cryptoCurrency!.name!.count))
+                firstTableCell.detailLabel.attributedText = detailAttributedText
+                setNavigationTitleWithAnimation(isSet: false)
+            }
         }
     }
 }
